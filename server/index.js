25ae347ae299a9fa2aa03afe00df2b7def2be77a
krins <krinskumar@gmail.com>
2024-12-06 01:03:37 -0500

    removed sockets

--- server/index.js
+++ server/index.js
@@ -1,5 +1,3 @@
-const WebSocket = require('ws');
-const server = new WebSocket.Server({ port: 8080 });
 const express = require('express');
 const app = express();
 const cors = require('cors');
@@ -17,24 +15,6 @@ function addValue(value) {
 
 addValue(Math.floor(Math.random() * 30));
 
-server.on('connection', function (ws) {
-    console.log('New device connected');
-
-    ws.on('message', function (data) {
-        try {
-            const sensorData = JSON.parse(data);
-            console.log('Received sensor data:', sensorData);
-            addValue(sensorData.value);
-        } catch (error) {
-            console.error('Error processing message:', error);
-        }
-    });
-
-    ws.on('close', function () {
-        console.log('Device disconnected');
-    });
-});
-
 app.get('/', (req, res) => {
     res.json(db);
 });
@@ -45,7 +25,6 @@ app.post('/', (req, res) => {
 });
 
 app.listen(3000, () => {
-    console.log('Server running on port 3000');
+    console.log('Server running on port 8080');
 });
 
-console.log('WebSocket server running on port 8080');
